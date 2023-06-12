884d12fee7fe3de8274f5676baa1e3472111feac
Mike <josemimore0@gmail.com>
2023-06-12 08:24:11 -0400

    added rules menus and function

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 //DOM
-const header = document.getElementById("header")
-const container = document.getElementById("container")
-const gameMenu  = document.getElementById("menu")
-const rulesMenu = document.getElementById('menu-rules');
-const playBtn = document.getElementById("play-btn")
-const imgs = document.getElementById("imgs")
-const startIMG = document.getElementById("start")
-const middleIMG = document.getElementById("middle")
-const endIMG = document.getElementById("end")
+const title = document.getElementById("title");
+const score = document.getElementById("score");
+const container = document.getElementById("container");
+const gameMenu = document.getElementById("menu");
+const rulesMenu = document.getElementById("menu-rules");
+const playBtn = document.getElementById("play-btn");
+const imgs = document.getElementById("imgs");
+const startIMG = document.getElementById("start");
+const middleIMG = document.getElementById("middle");
+const endIMG = document.getElementById("end");
 //Buttons
-const btnRight = document.getElementById("btn-right")
-const btnLeft = document.getElementById("btn-left")
-const btnUp = document.getElementById("btn-up")
-const btnDown = document.getElementById("btn-down")
-const btnRestart = document.getElementById("btn-restart")
-const radioButtons = document.getElementsByName('radio');
+const btnRestart = document.getElementById("btn-restart");
+const radioButtons = document.getElementsByName("radio");
+const checkButtons = document.getElementsByName("checkbox");
+//Player buttons
+const btnRight = document.getElementById("btn-right");
+const btnLeft = document.getElementById("btn-left");
+const btnUp = document.getElementById("btn-up");
+const btnDown = document.getElementById("btn-down");
 
-let blocks = 400;
+const rules = {
+    fly: false,
+    collision: false,
+    collisionSelf: false,
+    collisionBorders: false,
+};
+
+const UP = "up";
+const DOWN = "down";
+const RIGHT = "right";
+const LEFT = "left";
+const WORM = "worm";
+const SNAKE = "snake";
+const HIDDEN = "hidden";
+
+const BLOCKS = 400;
 let interval = 100;
 let apple = randomPosition();
 let fly = 399;
-let runningGame = 0
-let mode = "game"
+let runningGame = 0;
+let mode = "game";
 
-function getSelectedRadio() {
+function changeGameMode() {
     for (let radio of radioButtons) {
         if (radio.checked) {
             mode = radio.value;
@@ -31,51 +49,50 @@ function getSelectedRadio() {
     }
 }
 
+function changeRules() {
+    for (checkbox of checkButtons) {
+        rules[checkbox.id] = checkbox.checked;
+    }
+}
+
 function animation() {
-    startIMG.classList.add("hidden")
-    middleIMG.classList.remove("hidden")
+    startIMG.classList.add(HIDDEN);
+    middleIMG.classList.remove(HIDDEN);
     setTimeout(() => {
-        middleIMG.classList.add('hidden')
-        endIMG.classList.remove("hidden")
-        endIMG.classList.add('animation')
+        middleIMG.classList.add(HIDDEN);
+        endIMG.classList.remove(HIDDEN);
+        endIMG.classList.add("animation");
     }, 400);
-    
 }
 
 function startGame() {
-    animation()
+    animation();
     if (mode === "game") {
-       worm.changeBody([0,1,2])
+        worm.changeBody([0, 1, 2]);
     }
     setTimeout(() => {
-        container.classList.remove("hidden")
-        playBtn.classList.add("hidden")
-        gameMenu.classList.add("hidden")
-        rulesMenu.classList.add("hidden")
+        container.classList.remove(HIDDEN);
+        title.classList.add(HIDDEN);
+        playBtn.classList.add(HIDDEN);
+        gameMenu.classList.add(HIDDEN);
+        rulesMenu.classList.add(HIDDEN);
         runningGame = setInterval(`${mode}()`, interval);
     }, 2000);
 }
 
 function restart() {
-    clearInterval(runningGame)
+    clearInterval(runningGame);
     runningGame = setInterval(game, interval);
-}
-
-
-const rules = {
-    collision: true, 
-    collisionSelf: true,
-    collisionBorders: true,
 }
 
 const player = (name, body, direction) => {
     let points = 0;
 
-    const changeBody = (newBody) => body.splice(0, body.length, ...newBody);;
+    const changeBody = (newBody) => body.splice(0, body.length, ...newBody);
     const addPoint = () => points++;
     const getPoints = () => points;
     const getDirection = () => direction;
-    const changeDirection = (newDirection) => direction = newDirection; 
+    const changeDirection = (newDirection) => (direction = newDirection);
     const getName = () => name;
     const getLength = () => body.length;
     const getHead = () => Number(body.slice(-1));
@@ -83,226 +100,269 @@ const player = (name, body, direction) => {
     const move = () => {
         let nextMove = "";
         switch (direction) {
-            case "right":
-                nextMove = (getHead() > 398 ? getHead() - 19: getHead() + 1);
-                break
-            case "left":
-                nextMove = (getHead() < 1 ? getHead() + 19: getHead() - 1);
-                break
-            case "up":
-                nextMove = (getHead() < 20 ? getHead() + 380: getHead() - 20);
+            case RIGHT:
+                nextMove = getHead() > 398 ? getHead() - 19 : getHead() + 1;
                 break;
-            case "down":
-                nextMove = (getHead() > 379 ? getHead() - 380: getHead() + 20);
-                break
+            case LEFT:
+                nextMove = getHead() < 1 ? getHead() + 19 : getHead() - 1;
+                break;
+            case UP:
+                nextMove = getHead() < 20 ? getHead() + 380 : getHead() - 20;
+                break;
+            case DOWN:
+                nextMove = getHead() > 379 ? getHead() - 380 : getHead() + 20;
+                break;
         }
-        body.push(nextMove)
-        if (checkAppleEat() !== true) {
-            body.shift()
+        body.push(nextMove);
+        if (mode === "multiplayerGame") {
+            body.shift();
         }
-        
-    }
-    
-    const movePc = () => {
-        body.push(movePC(getHead))
-        body.shift
-    }
+        if (mode === "game" && checkAppleEat() !== true) {
+            body.shift();
+        }
+    };
 
-    return { body, getDirection, addPoint, getPoints, changeDirection, move, getName, getLength, getHead, movePc, changeBody }
+    const movePc = () => {
+        body.push(movePC(getHead));
+        body.shift;
+    };
+
+    return {
+        body,
+        getDirection,
+        addPoint,
+        getPoints,
+        changeDirection,
+        move,
+        getName,
+        getLength,
+        getHead,
+        movePc,
+        changeBody,
+    };
+};
+
+const worm = player(WORM, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], RIGHT);
+const snake = player(
+    SNAKE,
+    [399, 398, 397, 396, 395, 394, 393, 392, 391, 390],
+    LEFT
+);
+
+function randomPosition() {
+    return Number.parseInt(Math.random() * 399);
 }
 
-const worm = player("worm", [0, 1, 2, 3, 4, 5, 6 ,7 ,8, 9], "right");
-const snake = player("snake", [399, 398, 397, 396, 395, 394, 393, 392, 391, 390], "left");
-
-function randomPosition() {return Number.parseInt(Math.random() * 399) }
-
 function printTable() {
-    container.innerHTML = ""
-    for (let i = 0; i < blocks; i++) {
+    container.innerHTML = "";
+    for (let i = 0; i < BLOCKS; i++) {
         let div = document.createElement("div");
-        div.id = i
+        div.id = i;
         container.appendChild(div);
     }
 }
 
 function printPlayer(player) {
     for (let i = 0; i < player.getLength(); i++) {
-        document.getElementById(player.body[i]).classList.add(`${player.getName()}Body`)  
+        document
+            .getElementById(player.body[i])
+            .classList.add(`${player.getName()}Body`);
     }
-    document.getElementById(player.getHead()).classList.add(`${player.getName()}Head`)
+    document
+        .getElementById(player.getHead())
+        .classList.add(`${player.getName()}Head`);
 }
 
-
 function printApple() {
-    document.getElementById(apple).classList.add("apple")
+    document.getElementById(apple).classList.add("apple");
 }
 
 function printFly() {
-    document.getElementById(fly).classList.add("fly")
+    document.getElementById(fly).classList.add("fly");
 }
 
 function changeDirection(key) {
-    if (worm.getDirection() !== "left" && key === "ArrowRight" || this.id === "btn-right" ) {
-        worm.changeDirection("right")
-    } else if (worm.getDirection() !== "right" && key === "ArrowLeft" || this.id === "btn-left" ) {
-        worm.changeDirection("left")
-    } else if ( worm.getDirection() !== "up" && key === "ArrowDown" || this.id === "btn-down"  ) {
-        worm.changeDirection("down")
-    } else if ( worm.getDirection() !== "down" && key === "ArrowUp" || this.id === "btn-up" )  {
-        worm.changeDirection("up")
+    if (
+        (worm.getDirection() !== LEFT && key === "ArrowRight") ||
+        this.id === "btn-right"
+    ) {
+        worm.changeDirection(RIGHT);
+    } else if (
+        (worm.getDirection() !== RIGHT && key === "ArrowLeft") ||
+        this.id === "btn-left"
+    ) {
+        worm.changeDirection(LEFT);
+    } else if (
+        (worm.getDirection() !== UP && key === "ArrowDown") ||
+        this.id === "btn-down"
+    ) {
+        worm.changeDirection(DOWN);
+    } else if (
+        (worm.getDirection() !== DOWN && key === "ArrowUp") ||
+        this.id === "btn-up"
+    ) {
+        worm.changeDirection(UP);
     }
-    if (snake.getDirection()!== "left" && key === "d") {
-        snake.changeDirection("right")
-    } else if ( snake.getDirection() !== "right" && key === "a") {
-        snake.changeDirection("left")
-    } else if ( snake.getDirection() !== "up" && key === "s") {
-        snake.changeDirection("down")
-    } else if ( snake.getDirection() !== "down" && key === "w")  {
-        snake.changeDirection("up")
+    if (snake.getDirection() !== LEFT && key === "d") {
+        snake.changeDirection(RIGHT);
+    } else if (snake.getDirection() !== RIGHT && key === "a") {
+        snake.changeDirection(LEFT);
+    } else if (snake.getDirection() !== UP && key === "s") {
+        snake.changeDirection(DOWN);
+    } else if (snake.getDirection() !== DOWN && key === "w") {
+        snake.changeDirection(UP);
     }
-        
 }
 
 function checkLose(player) {
-    if (checkCollisionBorders(player)){
-        alert("chocaste")
-    }
-    // if (nextBox > 399 || nextBox < 0) {
-    //     interval = 0
-    //     count++
-    //     clearInterval(runningGame)
-    // } 
-    
-    // worm.body.forEach((box) => {
-    //     if(box === nextBox) {
-    //         count++
-    //         clearInterval(myInterval)
-    //     }
-    // })
+    // if (checkCollisionBorders(player)) {
+    //     alert("You die");
+    // }
+    // if (checkCollision(player)) {
+    //     alert("You eat enemy");
+    // }
+}
 
+function checkCollision(player) {
+    if (player.getName() === WORM) {
+        return worm.body.some((body) => body === snake.getHead());
+    } else {
+        return snake.body.some((body) => body === worm.getHead());
+    }
 }
 
 function checkCollisionBorders(player) {
-    
-    if (player.getDirection() === "up" && player.getHead() > 300 && player.body[player.getLength() - 2] < 20) {
-        return true
+    if (
+        player.getDirection() === UP &&
+        player.getHead() > 300 &&
+        player.body[player.getLength() - 2] < 20
+    ) {
+        return true;
     }
 
-    if (player.getDirection() === "down" && player.getHead() < 20 && player.body[player.getLength() - 2] > 300) {
-        return true
+    if (
+        player.getDirection() === DOWN &&
+        player.getHead() < 20 &&
+        player.body[player.getLength() - 2] > 300
+    ) {
+        return true;
     }
 
-    if (player.getDirection() === "right") {
-        let extreme = 20
+    if (player.getDirection() === RIGHT) {
+        let extreme = 20;
         for (let i = 0; i < 20; i++) {
             if (player.getHead() === extreme) {
-                return true
+                return true;
             }
-            extreme += 20
+            extreme += 20;
         }
-        
     }
 
-    if (player.getDirection() === "left") {
-        let extreme = 19
+    if (player.getDirection() === LEFT) {
+        let extreme = 19;
         for (let i = 0; i < 20; i++) {
             if (player.getHead() === extreme) {
-                return true
+                return true;
             }
-            extreme += 20
+            extreme += 20;
         }
     }
 }
 
 function checkAppleEat() {
     if (fly === apple) {
-        apple = randomPosition()
+        apple = randomPosition();
         return;
     }
     if (worm.getHead() === apple) {
-        apple = randomPosition()
-        worm.addPoint()
+        apple = randomPosition();
+        worm.addPoint();
         return true;
     }
 }
 
 function checkAppleEatMP() {
     if (fly === apple) {
-        apple = randomPosition()
+        apple = randomPosition();
         return;
     }
     if (worm.getHead() === apple) {
-        apple = randomPosition()
+        apple = randomPosition();
         snake.body.shift();
         return;
     }
     if (snake.getHead() === apple) {
         worm.body.shift();
-        apple = randomPosition()
+        apple = randomPosition();
         return;
-    } 
+    }
 }
-
 
 function game() {
     printTable();
-    
 
-    worm.move()
-    fly = movePC(fly);
-    checkAppleEat()
-    
-    printPlayer(worm)
-    printFly()
-    printApple()
+    if (rules.fly) {
+        fly = movePC(fly);
+        printFly();
+    }
+
+    worm.move();
+    checkAppleEat();
+    checkLose(worm);
+
+    printPlayer(worm);
+
+    printApple();
 }
 
 function multiplayerGame() {
-    printTable()
+    printTable();
 
     worm.move();
     snake.move();
-    fly = movePC(fly);
+
+    if (rules.fly) {
+        fly = movePC(fly);
+        printFly();
+    }
+
     checkAppleEatMP();
     checkLose(worm);
     checkLose(snake);
 
-    printApple()
-    printFly()
-    printPlayer(worm)
-    printPlayer(snake)
+    printApple();
+    printPlayer(worm);
+    printPlayer(snake);
 }
-
-
 
 function movePC(pc) {
     if (apple < pc) {
-        if ((pc - apple) > 15) {
-            pc -= 20
+        if (pc - apple > 15) {
+            pc -= 20;
         } else {
-            pc -= 1
+            pc -= 1;
         }
     }
     if (apple > pc) {
-        if ((apple - fly) > 15) {
-            pc += 20
+        if (apple - fly > 15) {
+            pc += 20;
         } else {
-            pc += 1
+            pc += 1;
         }
     }
-    return pc
+    return pc;
 }
 
-btnRight.addEventListener("click", changeDirection)
-btnLeft.addEventListener("click", changeDirection)
-btnUp.addEventListener("click", changeDirection)
-btnDown.addEventListener("click", changeDirection)
-btnRestart.addEventListener("click", restart)
-menu.addEventListener("click", getSelectedRadio)
-
+btnRight.addEventListener("click", changeDirection);
+btnLeft.addEventListener("click", changeDirection);
+btnUp.addEventListener("click", changeDirection);
+btnDown.addEventListener("click", changeDirection);
+btnRestart.addEventListener("click", restart);
+menu.addEventListener("click", changeGameMode);
+rulesMenu.addEventListener("click", changeRules);
+playBtn.addEventListener("click", startGame);
 //Keyboard handle
 document.addEventListener("keydown", (e) => {
-    e = e || window.event
-    changeDirection(e.key)
-    console.log(e.key)
-})
+    e = e || window.event;
+    changeDirection(e.key);
+});
